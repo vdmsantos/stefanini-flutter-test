7769a0ea0cbfcd94f81b96f5e0d52f65a2592ee9
Vitor Daniel Mendes dos Santos <94265037+mendesviton@users.noreply.github.com>
2024-06-19 01:53:07 -0300

    feat: add usecase interfaces

---
+++ lib/app/modules/core/interfaces/usecases/usecases.dart
@@ -0,0 +1,13 @@
+import '../types/type_defs.dart';
+
+abstract class UsecaseWithParams<Type, Params> {
+  const UsecaseWithParams();
+
+  ResultFuture<Type> call(Params params);
+}
+
+abstract class UsecaseWithoutParams<Type> {
+  const UsecaseWithoutParams();
+
+  ResultFuture<Type> call();
+}
